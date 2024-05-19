6bb12bc25d2316a01f5912ca813c8cc5b1242243
abraham <code.arb2712@gmail.com>
2024-05-18 21:27:09 -0400

    end

---
+++ src/app/components/Lines.tsx
@@ -0,0 +1,119 @@
+import React from "react";
+
+export const Lines = () => {
+  return (
+    <svg
+      width="430"
+      height="300"
+      viewBox="0 0 430 300"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M50 25H395V264H50V25Z" fill="#111111" />
+      <rect
+        x="86.4453"
+        y="-53"
+        width="87.3008"
+        height="154"
+        transform="rotate(48 86.4453 -53)"
+        fill="black"
+      />
+      <rect
+        x="412.445"
+        y="187"
+        width="87.3008"
+        height="154"
+        transform="rotate(48 412.445 187)"
+        fill="black"
+      />
+      <rect
+        x="-282.828"
+        y="341.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -282.828 341.416)"
+        fill="black"
+      />
+      <rect
+        x="-87.8281"
+        y="341.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -87.8281 341.416)"
+        fill="black"
+      />
+      <rect
+        x="15.1719"
+        y="428.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 15.1719 428.416)"
+        fill="black"
+      />
+      <rect
+        x="-179.828"
+        y="341.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -179.828 341.416)"
+        fill="black"
+      />
+      <rect
+        x="15.1719"
+        y="341.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 15.1719 341.416)"
+        fill="black"
+      />
+      <rect
+        x="118.172"
+        y="428.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 118.172 428.416)"
+        fill="black"
+      />
+      <rect
+        x="142.172"
+        y="491.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 142.172 491.416)"
+        fill="black"
+      />
+      <rect
+        x="-654.828"
+        y="428.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -654.828 428.416)"
+        fill="black"
+      />
+      <rect
+        x="-654.828"
+        y="341.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -654.828 341.416)"
+        fill="black"
+      />
+      <rect
+        x="-551.828"
+        y="428.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -551.828 428.416)"
+        fill="black"
+      />
+      <rect
+        x="-527.828"
+        y="491.416"
+        width="22.2488"
+        height="932.765"
+        transform="rotate(-133 -527.828 491.416)"
+        fill="black"
+      />
+    </svg>
+  );
+};
---
+++ src/app/page.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { Lines } from "./components/Lines";
+
+export default function Home() {
+  return (
+    <main className="grid lg:flex text-center overflow-hidden min-h-screen items-center justify-center lg:gap-[120px] relative">
+      <div className="flex justify-center -mt-[140px] lg:mb-0 lg:mt-0">
+        <figure className="lg:w-[300px] w-[200px]">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={300}
+            height={300}
+            className="w-full"
+          />
+        </figure>
+      </div>
+      <div className="text-white relative -mt-[400px]">
+        <h1 className="font-[100] text-[40px] lg:text-[80px] z-[100000] relative uppercase">
+          <span className="font-bold">estamos en</span> <br /> desarrollo
+        </h1>
+        <p className="text-[14px] lg:max-w-[380px] max-w-[280px] relative z-[1000]">
+          selecciona el servicio de preferencia y llena el formulario para poder
+          tener una reunion lo mas pronto posible.
+        </p>
+        <div className="absolute -bottom-[100px] -right-[100px]">
+          <Lines />
+        </div>
+      </div>
+      <div className="absolute bottom-8 text-center w-full">
+        <p className="text-white font-[100] text-[14px]">
+          univfeel derechos reservados
+        </p>
+      </div>
+    </main>
+  );
+}
